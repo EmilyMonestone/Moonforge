c6fbac6e800ac8a5955bd8896735dc41fe569ce7
Emily Pauli <github@emilypauli.de>
2025-10-26 09:53:31 +0100

    feat: add MultiProviderWrapper for managing multiple providers in the widget tree

---
+++ moonforge/lib/core/providers/providers.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:moonforge/core/providers/app_settings_provider.dart';
+import 'package:moonforge/core/providers/auth_providers.dart';
+import 'package:moonforge/features/campaign/controllers/campaign_provider.dart';
+import 'package:provider/provider.dart';
+
+class MultiProviderWrapper extends StatelessWidget {
+  final Widget child;
+
+  const MultiProviderWrapper({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    // Providers
+    AuthProvider authProvider = AuthProvider();
+    AppSettingsProvider appSettingsProvider = AppSettingsProvider();
+    CampaignProvider campaignProvider = CampaignProvider();
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthProvider>.value(value: authProvider),
+        ChangeNotifierProxyProvider<AuthProvider, AppSettingsProvider>(
+          create: (BuildContext context) {
+            return AppSettingsProvider();
+          },
+          update:
+              (
+                BuildContext context,
+                AuthProvider value,
+                AppSettingsProvider? previous,
+              ) {
+                appSettingsProvider.updateOnAuthChange(value);
+                return appSettingsProvider;
+              },
+        ),
+        ChangeNotifierProvider<CampaignProvider>.value(value: campaignProvider),
+      ],
+      child: child,
+    );
+  }
+}
