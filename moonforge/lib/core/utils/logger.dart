afe1d75cc2bb9384f2df655a359db0e5e9507409
Emily Pauli <github@emilypauli.de>
2025-10-29 10:37:53 +0100

    refactor: add documentation for core logger instance in logger.dart

--- moonforge/lib/core/utils/logger.dart
+++ moonforge/lib/core/utils/logger.dart
@@ -1,3 +1,10 @@
 import 'package:logger/logger.dart';
 
+/// Core logger instance
+/// Use this throughout the app for consistent logging
+/// Example:
+///  logger.i('Informational message');
+///  logger.e('Error message', error: exception, stackTrace: stackTrace);
+///  logger.d('Debug message');
+///  logger.w('Warning message');
 Logger logger = Logger(printer: PrettyPrinter());
